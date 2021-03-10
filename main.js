43dc51e9c459c5c8c5236216cdcd61ee0e41a159
saketoba7 <vtu6fle87@gmail.com>
2021-03-10 14:17:46 +0900

    second

---
+++ main.js
@@ -0,0 +1,9 @@
+$(document).on('ready', function() {
+  $(".variable").slick({
+    dots: true,
+    infinite: true,
+    variableWidth: true,
+    autoplay: true,
+    autoplaySpeed: 4000,
+  });
+});
